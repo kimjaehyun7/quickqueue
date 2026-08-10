33707a35f92b2b95bd9faa4177c1bdb83c27dc84
jaehyun <wogusrla77@naver.com>
2026-08-10 11:33:25 +0900

    first commit

---
+++ quickqueue/src/main/java/com/quickqueue/QuickqueueApplication.java
@@ -0,0 +1,13 @@
+package com.quickqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickqueueApplication.class, args);
+	}
+
+}
---
+++ quickqueue/src/test/java/com/quickqueue/QuickqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

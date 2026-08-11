223ecf8596121649fadfa0dde9ab0954a4ea01b5
jaehyun <wogusrla77@naver.com>
2026-08-11 12:27:03 +0900

    feat: 공용 시간 클래스 설정

---
+++ quickqueue/src/main/java/com/quickqueue/global/common/BaseCreatedTimeEntity.java
@@ -0,0 +1,20 @@
+package com.quickqueue.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class BaseCreatedTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
